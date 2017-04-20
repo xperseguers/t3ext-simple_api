b0c66fad475eff37fa25c4be411038c575a989b3
Xavier Perseguers <xavier@causal.ch>
2017-04-20 11:36:46 +0200

    [BUGFIX] Configure the TYPO3 caching

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,11 +2,19 @@
 defined('TYPO3_MODE') || die();
 
 $boot = function ($_EXTKEY) {
-    $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['simple_api']);
+    $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
     // Register API provider
     $eIDName = $settings['eIDName'];
     $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$eIDName] = 'EXT:' . $_EXTKEY . '/Classes/Controller/ApiController.php';
+
+    /*****************************************************
+     * API Caching
+     *****************************************************/
+
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$_EXTKEY])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$_EXTKEY] = [];
+    }
 };
 
 $boot($_EXTKEY);
