b65d8db3bdb68a49409ca9369e53df500cd15964
Xavier Perseguers <xavier@causal.ch>
2019-04-15 16:48:08 +0200

    [TASK] Kickstart compatibility with TYPO3 v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,8 +33,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.2.99',
-            'typo3' => '7.6.0-8.7.99',
+            'php' => '7.2.0-7.3.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
