5a4dbff7a8c705a885226abc272d24babedbba66
Xavier Perseguers <xavier@causal.ch>
2022-02-15 16:46:37 +0100

    [TASK] Kickstart compatibility with PHP 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.4.99',
+            'php' => '7.4.0-8.0.99',
             'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
