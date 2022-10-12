7ba1ccb70bfc1d5698dab54d6ff58e840e1ecbe3
Xavier Perseguers <xavier@causal.ch>
2022-10-12 13:40:20 +0200

    [TASK] Stick to PSR-12 for strict_types declaration

--- Classes/Service/HandlerService.php
+++ Classes/Service/HandlerService.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /*
  * This file is part of the TYPO3 CMS project.
  *
@@ -14,6 +12,8 @@ declare(strict_types=1);
  *
  * The TYPO3 project - inspiring people to share!
  */
+
+declare(strict_types=1);
 
 namespace Causal\SimpleApi\Service;
 
