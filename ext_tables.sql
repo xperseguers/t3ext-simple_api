79345a9e3d6afbbe0552cb11bce819989b6b7b5e
Xavier Perseguers <xavier@causal.ch>
2023-03-16 15:02:04 +0100

    [TASK] Drop useless DB definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,8 +2,9 @@
 # Table structure for table 'tx_simpleapi_cache_queue'
 #
 CREATE TABLE tx_simpleapi_cache_queue (
-	uid int(11) NOT NULL auto_increment,
+	uid int(11) unsigned NOT NULL auto_increment,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	cache_tag varchar(255) DEFAULT '' NOT NULL,
+
 	PRIMARY KEY (uid)
-) ENGINE=InnoDB;
+);
