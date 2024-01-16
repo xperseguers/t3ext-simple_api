3a8c39121f4d20091f6651400ed1386c9f7fd03e
Xavier Perseguers <xavier@causal.ch>
2024-01-16 12:48:54 +0100

    [TASK] Enable PHP 8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.2.99',
+            'php' => '7.4.0-8.3.99',
             'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
